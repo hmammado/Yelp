c1881ddbbb4c22fa86d7a3dfa8db25383fb01993
Haji Mammadov <hmammado@gmail.com>
2021-11-10 16:54:27 +0100

    Finished Restaurants

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,17 @@
+class ReviewsController < ApplicationController
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.save
+    redirect_to review_path
+  end
+
+end
